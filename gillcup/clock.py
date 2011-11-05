5287a404c6f030baaa4ef9dc3835c0bd54aef0cc
Petr Viktorin <encukou@gmail.com>
2011-11-06 01:14:49 +0200

    Scheduling events

--- gillcup/clock.py
+++ gillcup/clock.py
@@ -1,6 +1,75 @@
+# Encoding: UTF-8
+
+import collections
+import weakref
+import heapq
+
+_HeapEntry = collections.namedtuple('EventHeapEntry', 'time index action')
+
 class Clock(object):
+    """Keeps track of time.
+
+    In Gillcup, animation means two things: running code at specified times,
+    and changing object properties with time.
+
+    You will notice that the preceding sentence mentions time quite a lot. But
+    what is this time?
+
+    You could determine time by looking at the computer's clock, but that would
+    only work with real-time animations. When you'd want to render a movie,
+    where each frame takes 2 seconds to draw and there are 25 frames per
+    second, you'd be stuck.
+
+    That's why Gillcup introduces a flexible source of time: the Clock. This is
+    an objects with three basic attributes:
+
+    -  time, which gives the current time (“now”) on the clock,
+    -  advance(dt), which advances the timer by “dt” units, and
+    -  schedule(dt, action), which schedules “action” to be called “dt” time
+       units from the current time.
+
+    What a “time unit” means is entirely up to the application – it could be
+    seconds, movie/simulation frames, etc.
+    """
     def __init__(self, time=0):
+        # Time on the clock
         self.time = time
 
+        # Next action index; used to keep FIFO ordering for actions scheduled
+        # for the same time
+        self.next_index = 0
+
+        # Heap queue of scheduled actions
+        self.events = []
+
     def advance(self, dt):
+        """Call to advance the clock's time
+
+        Steps the clock dt units to the future, pausing at times when actions
+        are scheduled, and running them.
+
+        Attempting to move to the past (dt<0) will raise an error.
+        """
+        if dt < 0:
+            raise ValueError('Moving backwards in time')
+        while self.events and self.events[0].time <= self.time + dt:
+            entry = heapq.heappop(self.events)
+            dt -= entry.time - self.time
+            self.time = entry.time
+            entry.action()
         self.time += dt
+
+    def schedule(self, action, dt=0):
+        """Schedule an action to be run "dt" time units from the current time
+
+        Scheduling is stable:  if two things are scheduled for the same
+        time, they will be called in the order they were scheduled.
+
+        Scheduling an action in the past (dt<0) will raise an error.
+        """
+        if dt < 0:
+            raise ValueError('Scheduling an action in the past')
+        self.next_index += 1
+        scheduled_time = self.time + dt
+        entry = _HeapEntry(scheduled_time, self.next_index, action)
+        heapq.heappush(self.events, entry)
--- gillcup/test/test_clock.py
+++ gillcup/test/test_clock.py
@@ -1,7 +1,70 @@
+from __future__ import division
+
+import pytest
+
 from gillcup import Clock
+
+def dummy_function():
+    pass
+
+def append_const(lst, value):
+    def append():
+        lst.append(value)
+    return append
+
+def append_time(lst, clock):
+    def append():
+        lst.append(clock.time)
+    return append
 
 def test_clock():
     clock = Clock()
     assert clock.time == 0
     clock.advance(1)
     assert clock.time == 1
+
+def test_scheduling():
+    clock = Clock()
+    lst = []
+    clock.schedule(append_const(lst, 'a'), 1)
+    clock.schedule(append_const(lst, 'd'), 3)
+    clock.schedule(append_const(lst, 'b'), 1)
+    clock.schedule(append_const(lst, 'c'), 1)
+    clock.advance(0)
+    assert lst == []
+    clock.advance(1)
+    assert lst == ['a', 'b', 'c']
+    clock.advance(1)
+    assert lst == ['a', 'b', 'c']
+    clock.advance(1)
+    assert lst == ['a', 'b', 'c', 'd']
+
+def test_negative_dt():
+    clock = Clock()
+    with pytest.raises(ValueError):
+        clock.advance(-1)
+    with pytest.raises(ValueError):
+        clock.schedule(dummy_function, -1)
+
+def test_zero_dt():
+    clock = Clock()
+    lst = []
+    clock.schedule(append_const(lst, 'a'), 0)
+    clock.advance(0)
+    assert lst == ['a']
+
+def test_integer_times():
+    """
+    Ensure that even if the clock advances by float ticks, events scheduled
+    at integer times get fired with clock.time being an integer.
+    """
+    clock = Clock()
+    lst = []
+    clock.schedule(append_time(lst, clock), 1)
+    clock.schedule(append_time(lst, clock), 2)
+    clock.schedule(append_time(lst, clock), 3)
+    for i in xrange(30):
+        clock.advance(0.3)
+    assert lst == [1, 2, 3]
+    for time in lst:
+        assert type(time) == int
