ae1bf1078270955ceccae3fc91021dac94aa4dc1
Petr Viktorin <encukou@gmail.com>
2011-11-07 01:31:21 +0200

    Add a forgotten file to git

---
+++ gillcup/effect.py
@@ -0,0 +1,14 @@
+
+class Effect(object):
+    """Object that changes an AnimatedProperty
+
+    Effects should have a `value` attribute that provides a value for the
+    property.
+    """
+    pass
+
+class ConstantEffect(Effect):
+    """An Effect that provides a constant value
+    """
+    def __init__(self, value):
+        self.value = value
