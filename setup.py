c3cd7b8886760224509f6f3a089bdb290649354e
Petr Viktorin <encukou@gmail.com>
2014-08-03 14:13:20 +0200

    We're way past Python 3.2 now

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ setup_args = dict(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries',
     ],
 
