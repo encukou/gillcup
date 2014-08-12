47a0b0e9ecb0e73701d996ad48c0c1f682b89e6b
Petr Viktorin <encukou@gmail.com>
2014-08-12 22:49:22 +0200

    Test that AnimatedProperty behaves like a simple attribute, where possible

--- test_gillcup/conftest.py
+++ test_gillcup/conftest.py
@@ -18,15 +18,11 @@ def subclock():
 
 @pytest.yield_fixture
 def check_dump():
-    called = False
 
     def check_dump(expression, expected):
-        nonlocal called
-        called = True
         dumped = dump(expression)
         print(dumped)
         expected = textwrap.dedent(expected.strip('\n').rstrip())
         assert dumped == expected
 
     yield check_dump
-    assert called, 'check_dump was not called'
--- test_gillcup/test_properties.py
+++ test_gillcup/test_properties.py
@@ -1,19 +1,35 @@
+import contextlib
+
 import pytest
 
 from gillcup.properties import AnimatedProperty, link
 from gillcup.expressions import Progress
 
 
-class Buzzer:
+class BuzzerBase:
+    def __repr__(self):
+        return '<test buzzer>'
+
+    @contextlib.contextmanager
+    def extra_behavior(self, behavior_type):
+        """Wraps test code that uses special AnimatedProperty features
+
+        Usage is similar to :func:`pytest.raises`.
+
+        See :meth:`NaïveBuzzer.extra_behavior` code for possible values of
+        behavior_type, and their explanations
+        """
+        yield
+
+
+class Buzzer(BuzzerBase):
+    """The basic buzzer"""
     volume = AnimatedProperty(0, name='volume')
     pitch = AnimatedProperty(440, name='pitch')
     position = x, y, z = AnimatedProperty(0, 0, 0, name='position: x y z')
 
-    def __repr__(self):
-        return '<test buzzer>'
 
-
-class MultichannelBuzzer:
+class MultichannelBuzzer(BuzzerBase):
     """A Buzzer that emits 3 simultaneous tones
 
     Not good programming practice!
@@ -26,11 +42,46 @@ class MultichannelBuzzer:
         440, 440, 440, name='pitches: pitch pitch2 pitch3')
     position = x, y, z = AnimatedProperty(0, 0, 0, name='position: x y z')
 
-    def __repr__(self):
-        return '<test buzzer>'
+
+class NaïveBuzzer(BuzzerBase):
+    """A Buzzer that uses simple values/expressions instead of AnimatedProperty
+
+    This is here to test that AnimatedProperty behaves like a simple attribute
+    wherever possible.
+    """
+    volume = 0
+    pitch = 440
+    position = x, y, z = 0, 0, 0
+
+    @contextlib.contextmanager
+    def extra_behavior(self, behavior_type):
+        if behavior_type == 'link function':
+            # The link() function can be used on an AnimatedProperty value
+            with pytest.raises(TypeError) as e:
+                yield
+            assert str(e.value) == "<class 'int'> can't be linked"
+        elif behavior_type == 'link method':
+            # AnimatedProperty values have a link() method
+            with pytest.raises(AttributeError) as e:
+                yield
+            assert str(e.value) == "'int' object has no attribute 'link'"
+        elif behavior_type == 'syncs-components':
+            # Tuple AnimatedProperty values sync with their components
+            with pytest.raises(AssertionError) as e:
+                yield
+        elif behavior_type == 'is-expression':
+            # AnimatedProperty values are always expressions
+            with pytest.raises(AssertionError) as e:
+                yield
+        else:
+            raise LookupError(behavior_type)
 
 
-@pytest.fixture(params=[Buzzer, MultichannelBuzzer])
+buzzer_subclasses = [Buzzer, MultichannelBuzzer, NaïveBuzzer]
+
+
+@pytest.fixture(params=buzzer_subclasses,
+                ids=[c.__name__ for c in buzzer_subclasses])
 def buzzer_class(request):
     return request.param
 
@@ -50,12 +101,25 @@ def test_defaults(buzzer):
 def test_setting_scalar(buzzer):
     buzzer.volume = 50
     assert buzzer.volume == 50
-    assert buzzer.volume == (50,)
+    with buzzer.extra_behavior('is-expression'):
+        assert buzzer.volume == (50,)
 
 
 def test_setting_vector(buzzer):
     buzzer.position = 1, 2, 3
     assert buzzer.position == (1, 2, 3)
+    with buzzer.extra_behavior('syncs-components'):
+        assert buzzer.x == 1
+        assert buzzer.y == 2
+        assert buzzer.z == 3
+
+
+def test_setting_component(buzzer):
+    buzzer.x = 1
+    buzzer.y = 2
+    buzzer.z = 3
+    with buzzer.extra_behavior('syncs-components'):
+        assert buzzer.position == (1, 2, 3)
     assert buzzer.x == 1
     assert buzzer.y == 2
     assert buzzer.z == 3
@@ -151,42 +215,45 @@ def test_set_to_property_divergent(buzzer, clock):
 
 
 def test_set_to_property_linked_method(buzzer, clock):
-    buzzer.volume.link(buzzer.pitch)
-    buzzer.pitch += Progress(clock, 2) * 20
-    assert buzzer.volume == 440
-    assert buzzer.pitch == 440
-    clock.advance_sync(1)
-    assert buzzer.volume == 450
-    assert buzzer.pitch == 450
-    clock.advance_sync(1)
-    assert buzzer.volume == 460
-    assert buzzer.pitch == 460
+    with buzzer.extra_behavior('link method'):
+        buzzer.volume.link(buzzer.pitch)
+        buzzer.pitch += Progress(clock, 2) * 20
+        assert buzzer.volume == 440
+        assert buzzer.pitch == 440
+        clock.advance_sync(1)
+        assert buzzer.volume == 450
+        assert buzzer.pitch == 450
+        clock.advance_sync(1)
+        assert buzzer.volume == 460
+        assert buzzer.pitch == 460
 
 
 def test_set_to_property_linked_function(buzzer, clock):
-    buzzer.volume = link(buzzer.pitch)
-    buzzer.pitch += Progress(clock, 2) * 20
-    assert buzzer.volume == 440
-    assert buzzer.pitch == 440
-    clock.advance_sync(1)
-    assert buzzer.volume == 450
-    assert buzzer.pitch == 450
-    clock.advance_sync(1)
-    assert buzzer.volume == 460
-    assert buzzer.pitch == 460
+    with buzzer.extra_behavior('link function'):
+        buzzer.volume = link(buzzer.pitch)
+        buzzer.pitch += Progress(clock, 2) * 20
+        assert buzzer.volume == 440
+        assert buzzer.pitch == 440
+        clock.advance_sync(1)
+        assert buzzer.volume == 450
+        assert buzzer.pitch == 450
+        clock.advance_sync(1)
+        assert buzzer.volume == 460
+        assert buzzer.pitch == 460
 
 
 def test_set_to_expression_with_linked_property(buzzer, clock):
-    buzzer.volume = link(buzzer.pitch)
-    buzzer.pitch += Progress(clock, 2) * 20
-    assert buzzer.volume == 440
-    assert buzzer.pitch == 440
-    clock.advance_sync(1)
-    assert buzzer.volume == 450
-    assert buzzer.pitch == 450
-    clock.advance_sync(1)
-    assert buzzer.volume == 460
-    assert buzzer.pitch == 460
+    with buzzer.extra_behavior('link function'):
+        buzzer.volume = link(buzzer.pitch)
+        buzzer.pitch += Progress(clock, 2) * 20
+        assert buzzer.volume == 440
+        assert buzzer.pitch == 440
+        clock.advance_sync(1)
+        assert buzzer.volume == 450
+        assert buzzer.pitch == 450
+        clock.advance_sync(1)
+        assert buzzer.volume == 460
+        assert buzzer.pitch == 460
 
 
 def test_recursive_link(buzzer, buzzer_class, clock):
@@ -194,65 +261,69 @@ def test_recursive_link(buzzer, buzzer_class, clock):
         # TODO: This gets rather nasty with component property; fix!
         raise pytest.xfail(
             'INTERNALERROR> RuntimeError: maximum recursion depth exceeded')
-    buzzer.volume = link(buzzer.volume)
-    with pytest.raises(RuntimeError):
-        buzzer.volume.get()
-    assert str(buzzer.volume) == '<RuntimeError while getting value>'
+    with buzzer.extra_behavior('link function'):
+        buzzer.volume = link(buzzer.volume)
+        with pytest.raises(RuntimeError):
+            buzzer.volume.get()
+        assert str(buzzer.volume) == '<RuntimeError while getting value>'
 
 
 def test_link_dump_method(buzzer, buzzer_class, clock, check_dump):
     # The dump is different for normal properties and for components
-    buzzer.volume.link(buzzer.pitch)
-    if buzzer_class == Buzzer:
-        check_dump(buzzer.volume, """
-            <test buzzer>.volume value <440.0>:
-              linked <test buzzer>.pitch <440.0>:
-                Constant <440.0>
-        """)
-    elif buzzer_class == MultichannelBuzzer:
-        check_dump(buzzer.volume, """
-            <test buzzer>.volume (volumes[0]) value <440.0>:
-              <test buzzer>.volumes value <440.0, 0.0, 0.0>:
-                Concat <440.0, 0.0, 0.0>:
-                  linked <test buzzer>.pitch (pitches[0]) <440.0>:
-                    <test buzzer>.pitches value <440.0, 440.0, 440.0>:
-                      Constant <440.0, 440.0, 440.0>
-                  Constant <0.0, 0.0>
-        """)
-    else:
-        raise TypeError(buzzer_class)
+    with buzzer.extra_behavior('link method'):
+        buzzer.volume.link(buzzer.pitch)
+        if buzzer_class == Buzzer:
+            check_dump(buzzer.volume, """
+                <test buzzer>.volume value <440.0>:
+                  linked <test buzzer>.pitch <440.0>:
+                    Constant <440.0>
+            """)
+        elif buzzer_class == MultichannelBuzzer:
+            check_dump(buzzer.volume, """
+                <test buzzer>.volume (volumes[0]) value <440.0>:
+                  <test buzzer>.volumes value <440.0, 0.0, 0.0>:
+                    Concat <440.0, 0.0, 0.0>:
+                      linked <test buzzer>.pitch (pitches[0]) <440.0>:
+                        <test buzzer>.pitches value <440.0, 440.0, 440.0>:
+                          Constant <440.0, 440.0, 440.0>
+                      Constant <0.0, 0.0>
+            """)
+        else:
+            raise TypeError(buzzer_class)
 
 
 def test_link_dump_function(buzzer, buzzer_class, clock, check_dump):
     # The dump is different for normal properties and for components
-    buzzer.volume = link(buzzer.pitch) + 4
-    if buzzer_class == Buzzer:
-        check_dump(buzzer.volume, """
-            <test buzzer>.volume value <444.0>:
-              + <444.0>:
-                linked <test buzzer>.pitch <440.0>:
-                  Constant <440.0>
-                Constant <4.0>
-        """)
-    elif buzzer_class == MultichannelBuzzer:
-        check_dump(buzzer.volume, """
-            <test buzzer>.volume (volumes[0]) value <444.0>:
-              <test buzzer>.volumes value <444.0, 0.0, 0.0>:
-                Concat <444.0, 0.0, 0.0>:
+    with buzzer.extra_behavior('link function'):
+        buzzer.volume = link(buzzer.pitch) + 4
+        if buzzer_class == Buzzer:
+            check_dump(buzzer.volume, """
+                <test buzzer>.volume value <444.0>:
                   + <444.0>:
-                    linked <test buzzer>.pitch (pitches[0]) <440.0>:
-                      <test buzzer>.pitches value <440.0, 440.0, 440.0>:
-                        Constant <440.0, 440.0, 440.0>
+                    linked <test buzzer>.pitch <440.0>:
+                      Constant <440.0>
                     Constant <4.0>
-                  Constant <0.0, 0.0>
-        """)
-    else:
-        raise TypeError(buzzer_class)
+            """)
+        elif buzzer_class == MultichannelBuzzer:
+            check_dump(buzzer.volume, """
+                <test buzzer>.volume (volumes[0]) value <444.0>:
+                  <test buzzer>.volumes value <444.0, 0.0, 0.0>:
+                    Concat <444.0, 0.0, 0.0>:
+                      + <444.0>:
+                        linked <test buzzer>.pitch (pitches[0]) <440.0>:
+                          <test buzzer>.pitches value <440.0, 440.0, 440.0>:
+                            Constant <440.0, 440.0, 440.0>
+                        Constant <4.0>
+                      Constant <0.0, 0.0>
+            """)
+        else:
+            raise TypeError(buzzer_class)
 
 
 def test_link_function_idempotent(buzzer, clock):
-    linked = link(buzzer.volume)
-    assert linked is link(linked)
+    with buzzer.extra_behavior('link function'):
+        linked = link(buzzer.volume)
+        assert linked is link(linked)
 
 
 def test_default_naming():
