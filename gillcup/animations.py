425d2d3b329bba4ddeb83f0583be20420f86f050
Petr Viktorin <encukou@gmail.com>
2014-08-14 00:54:00 +0200

    animations.anim: Add the `delay` argument

--- gillcup/animations.py
+++ gillcup/animations.py
@@ -11,7 +11,7 @@ Reference
 from gillcup.expressions import Interpolation, Progress
 
 
-def anim(start, end, duration, clock):
+def anim(start, end, duration, clock, *, delay=0):
     """Create an animated expression
 
     Returns an expression that morphs between :token:`start` and :token:`end`
@@ -20,13 +20,17 @@ def anim(start, end, duration, clock):
     At its simplest, animation is a simple combination of the
     :class:`~gillcup.expressions.Interpolation` and
     :class:`~gillcup.expressions.Progress` expressions.
-    This function wraps creating them in an easy-to-use package.
+    This function wraps creating them in an easy-to-use package,
+    with a few extra frills on top.
+
+    The resulting animation is scheduled as soon as it is created.
 
     :param start: The value at the start
     :param end: The value at the end
     :param duration: The duration of the animation
     :param clock: The :class:`~gillcup.clock.Clock` that controls the
                   animation's time
+    :param delay: Time at which the animation starts
     """
-    progress = Progress(clock, duration)
+    progress = Progress(clock, duration, delay=delay)
     return Interpolation(start, end, progress)
--- test_gillcup/test_animations.py
+++ test_gillcup/test_animations.py
@@ -8,6 +8,8 @@ def test_anim_basic(clock):
     assert animation == 2
     clock.advance_sync(1)
     assert animation == 3
+    clock.advance_sync(1)
+    assert animation == 3
 
 
 def test_anim_basic_vector(clock):
@@ -17,3 +19,16 @@ def test_anim_basic_vector(clock):
     assert animation == (2, 3)
     clock.advance_sync(1)
     assert animation == (3, 4)
+
+
+def test_anim_delay(clock):
+    animation = anim(1, 3, 2, clock, delay=1)
+    assert animation == 1
+    clock.advance_sync(1)
+    assert animation == 1
+    clock.advance_sync(1)
+    assert animation == 2
+    clock.advance_sync(1)
+    assert animation == 3
+    clock.advance_sync(1)
+    assert animation == 3
